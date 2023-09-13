846fc23e19e5829671ab4156ce58624996371fca
Adailo Melo <studyadailo@gmail.com>
2023-09-13 14:31:07 -0300

    Create ex002.js

---
+++ 001-JavaScript-Basico/ex002.js
@@ -0,0 +1,7 @@
+const nome = "Adailo de Melo Sousa";
+let idade = 20;
+let peso = 84;
+let altura = 1.60
+let imc = peso * (altura ** 2)
+
+console.log(`${nome} tem ${idade} anos, pesa ${peso} Kg tem ${altura}m de altura e seu IMC é de ${imc}`)
