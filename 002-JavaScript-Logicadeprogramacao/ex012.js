7a21e6bdecfbe4ec1ed05736ebe3c7fbd9f5cf1e
Adailo Melo <studyadailo@gmail.com>
2023-09-20 11:22:03 -0300

    Create ex012.js

---
+++ 002-JavaScript-Logicadeprogramacao/ex012.js
@@ -0,0 +1,9 @@
+//Escreva uma função que recebe dois números e retorna o maior deles
+
+const maior = (x, y) => x > y ? x : y; //Função com dois parâmetros que vê qual número é maior
+
+function main(){
+    console.log(maior(7, 15));
+}
+
+main();
